e26ef86cb4c547df2e691d19a147bea8a6d0bebf
lazzaric92 <lazzaric_92@hotmail.it>
2024-05-09 21:06:43 +0200

    added img, css content

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@
 const userDistanceInputEl = document.querySelector('input#user-distance');
 const userAgeInputEl = document.querySelector('input#user-age');
 const formButtonEl = document.querySelector('div.user-form>button');
+const userDiscountEl = document.getElementById('user-discount');
+const ticketPriceEl = document.getElementById('ticket-price');
 let userDistance;
 let userAge;
 const pricePerKm = 0.267113;
@@ -19,12 +21,21 @@ formButtonEl.addEventListener('click', function(){
     let discount = 0;
     if (userAge < 21){
         discount = 24.552;
+        userDiscountEl.classList.remove('d-none');
     } else if (userAge >= 63){
         discount = 37.893;
+        userDiscountEl.classList.remove('d-none');
     }
 
     const ticketFullPrice = userDistance * pricePerKm;
     ticketPrice = (ticketFullPrice - (ticketFullPrice /100) * discount);
     
     console.log(ticketPrice.toFixed(2));
+    if(ticketPrice > 0) {
+        ticketPriceEl.classList.remove('d-none');
+        ticketPriceEl.innerHTML = 'Il prezzo del tuo biglietto è: ' + ticketPrice.toFixed(2) + ' €';
+    } else {
+        ticketPriceEl.classList.remove('d-none');
+        ticketPriceEl.innerHTML = 'Si è verificato un errore';
+    }   
 });
